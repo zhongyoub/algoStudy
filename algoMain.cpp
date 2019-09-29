6afceb7317c64c1e512d608e4ab847fa32d77d7e
zhongyoub <1143747780@qq.com>
2019-09-29 23:24:02 +0800

    main.cpp

---
+++ algoMain.cpp
@@ -0,0 +1,14 @@
+#include "algo_one.h"
+#include "algo_two.h"
+#include "algo_three.h"
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	int a[] = { 2,1,4,7,3,2,1,5 };
+	int num = 0;
+	num = getMaxMoutain(a, 8);
+	return 0;
+}
