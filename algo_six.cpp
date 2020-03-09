db0f27d78b0104c4bbe6ca8d9bb2c69d45ec5102
zhongyoub <1143747780@qq.com>
2020-03-09 13:48:08 +0800

    202003091348

---
+++ algo_six.cpp
@@ -0,0 +1,94 @@
+﻿#include<iostream>
+#include<map>
+#include<string>
+
+using namespace std;
+
+/*
+【字符串类型】给一串数字，输出这些数字能够组成的合法的最大时间，数字不够则用零补全
+例：
+输入：“274536”
+输出：“23:57:46”
+*/
+
+map<int, int>mm;
+string res = "";
+
+//
+int get(int mx) {
+	int i = mx;
+	for (; i >= 0; i--) {
+		if (mm[i] > 0) {
+			mm[i]--;
+			return i;
+		}
+	}
+	return  - 1;
+}
+
+void init() {
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		mm[i] = 0;
+	}
+}
+
+void push(int k) {
+	if (-1 != k) {
+		res.push_back(k + '0');
+	}
+	else {
+		res.push_back('0');
+	}
+}
+
+bool string_check(string s)
+{
+	int length = s.length();
+	for (int i = 0; i < length; i++)
+	{
+		int temp = (int)s[i];
+		if (temp >= 48 && temp <= 57)
+		{
+			continue;
+		}
+		else
+		{
+			return false;
+		}
+	}
+		return true;
+}
+
+int main()
+{
+	string s;
+	//输入数字字符串
+	cin >> s;
+	//检查输入是否全为数字
+	if (!string_check(s))
+		return -1;
+	int len  = s.length();
+	int i, j, k, l;
+	// 字符串中每个字符转换为map的key,value是数字的个数
+	for (i = 0; i < len; i++) {
+		mm[(int)(s[i] - '0')]++;
+	}
+	k  = get(2);
+	push(k);
+	if (2 == k) {
+		push(get(3));
+	}
+	else {
+		push(get(9));
+	}
+	res.push_back(':');
+	push(get(5));
+	push(get(9));
+	res.push_back(':');
+	push(get(5));
+	push(get(9));
+	cout << res << endl;
+	return 0;
+}
---
+++ coinChange.cpp
@@ -0,0 +1,59 @@
+#include<iomanip>
+#include<iostream>
+using namespace std;
+
+int coin_value[] = { 1, 2, 5, 10 };
+int coin_count[] = { 0, 0, 0, 0 };
+int entry_count;
+int* matrix;
+int cc(int money, int coin_type, int lr)
+{
+	int val = 0;
+	int vala = 0;
+	int valb = 0;
+	entry_count++;
+	//    cout<<"("<< setw(4) <<money<<","<< setw(4) <<coin_value[coin_type]<<")  ==> ";
+	if (lr == 0)
+	{
+		int a = sizeof(coin_count);
+		memset(coin_count, 0, sizeof(coin_count));
+	}
+	if (money == 0)
+	{
+		val = 1;
+	}
+	else if ((money < 0) || (coin_type == 0))
+	{
+		val = 0;
+	}
+	//    else if (matrix[money*6+coin_type] >=0)
+	//    {
+	//        val = matrix[money*6+coin_type];
+	//    }
+	else
+	{
+		vala = cc(money, coin_type - 1, 0);
+		valb = cc(money - coin_value[coin_type-1], coin_type, 1);
+		val = vala + valb;
+		if (vala == 0) coin_count[coin_type] += valb;
+		else           coin_count[coin_type] = valb;
+		if (money >= 0)matrix[money * 4 + coin_type] = val;
+	}
+
+	//    cout<<"("<< setw(4) <<money<<","<< setw(4) <<coin_value[coin_type]<<", ";
+	//    cout<<setw(4) << val <<", "<<(lr==0? "left" :"right") <<")"<<endl;
+	return val;
+}
+
+int main()
+{
+	int money = 100;
+	matrix = new int[(money + 1) * sizeof(coin_value) / sizeof(int)];
+	memset(matrix, -1, (money + 1) * sizeof(coin_value));
+	entry_count = 0;
+	cout << "method = " << cc(money, 4, 0);
+	cout << ", calls= " << entry_count << endl;
+	delete[] matrix;
+	system("pause");
+	return 0;
+}
